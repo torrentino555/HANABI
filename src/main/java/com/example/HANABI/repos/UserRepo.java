c1b7e43df197513fee5bcd26b63f205f0541a82a
kirillovcinnikov <kirock.98@mail.ru>
2019-06-29 19:21:41 +0300

    ..

--- src/main/java/com/example/HANABI/repos/UserRepo.java
+++ src/main/java/com/example/HANABI/repos/UserRepo.java
@@ -1,8 +1,9 @@
 package com.example.HANABI.repos;
 
+import com.example.HANABI.domain.User;
 import com.example.HANABI.domain.UserProjects;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<UserProjects, Long> {
-    UserProjects findByUsername(String username);
+    User findByUsername(String username);
 }
